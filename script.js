04b2d8a4e6430e1def00984c5c85cc4e585d0941
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-05-04 19:50:36 +0330

    swiper

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 //Swiper 
-const swiper = new Swiper('.bannerSwiper', {
-    spaceBetween:0,
-    effect:"fade",  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable:true
+// create thumbs swiper
+let thumbsSwiper = new Swiper('.thumbsSwiper', {
+  spaceBetween: 10,
+  slidesPerView: 5,
+  breakpoints: {
+    200: {
+      slidePreView: 1.5
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  });
+    400: {
+      slidePreView: 1.5
+    },
+    600: {
+      slidePreView: 3
+    },
+    1100: {
+      slidePreView: 5
+    },
+  },
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+
+const swiper = new Swiper('.bannerSwiper', {
+  spaceBetween: 0,
+  effect: "fade",
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  //make thumbs slider 
+  thumbs: {
+    swiper: thumbsSwiper
+  }
+
+});
