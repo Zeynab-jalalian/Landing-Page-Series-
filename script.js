b218f31ce45a5cc2b4e4ce129d5517dec20db98c
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-05-05 11:20:00 +0330

    dynamic titles

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 //Data
-let titles={
-  0:{
-    title:"Breaking Bad",
-    subTitle:" Crime Drama",
-    desc:"A high school chemistry teacher dying of cancer teams with a former student to secure his family's future by manufacturing and selling crystal ...",
-    videoURL:"videos/1.mp4"
+let titles = {
+  0: {
+    title: "Breaking Bad",
+    subTitle: " Crime Drama",
+    desc: "A high school chemistry teacher dying of cancer teams with a former student to secure his family's future by manufacturing and selling crystal ...",
+    videoURL: "videos/1.mp4"
   },
-  1:{
-    title:"The Walking Dead",
-    subTitle:"Post-apocalyptic Horror Drama",
-    desc:"Sheriff Deputy Rick Grimes wakes up from a coma to learn the world is in ruins and must lead a group of survivors to stay alive.Sheriff Deputy Rick Grimes ...",
-     videoURL:"videos/2.mp4"
+  1: {
+    title: "The Walking Dead",
+    subTitle: "Post-apocalyptic Horror Drama",
+    desc: "Sheriff Deputy Rick Grimes wakes up from a coma to learn the world is in ruins and must lead a group of survivors to stay alive.Sheriff Deputy Rick Grimes ...",
+    videoURL: "videos/2.mp4"
   },
-  2:{
-    title:"Game of Thrones",
-    subTitle:"Fantasy Drama",
-    desc:"It's the depiction of two powerful families -- kings and queens, knights and renegades, liars and honest men -- playing a deadly game for control of the Seven ...",
-    videoURL:"videos/3.mp4"
+  2: {
+    title: "Game of Thrones",
+    subTitle: "Fantasy Drama",
+    desc: "It's the depiction of two powerful families -- kings and queens, knights and renegades, liars and honest men -- playing a deadly game for control of the Seven ...",
+    videoURL: "videos/3.mp4"
   },
-  3:{
-    title:"The Punisher",
-    subTitle:"َAction",
-    desc:"Frank Castle was an honest ex-Marine with a family he loved. But when that family was murdered, he became a vigilante with a uniquely brutal brand of justice...",
- videoURL:"videos/4.mp4"
+  3: {
+    title: "The Punisher",
+    subTitle: "َAction",
+    desc: "Frank Castle was an honest ex-Marine with a family he loved. But when that family was murdered, he became a vigilante with a uniquely brutal brand of justice...",
+    videoURL: "videos/4.mp4"
   },
-  4:{
-    title:"Squid Game",
-    subTitle:"Thriller, Horror",
-    desc:"It revolves around a secret contest where 456 players, all of whom are in deep financial hardship, risk their lives to play a series of deadly children's games ...",
- videoURL:"videos/5.mp4"
+  4: {
+    title: "Squid Game",
+    subTitle: "Thriller, Horror",
+    desc: "It revolves around a secret contest where 456 players, all of whom are in deep financial hardship, risk their lives to play a series of deadly children's games ...",
+    videoURL: "videos/5.mp4"
   },
-  5:{
-    title:"Anne with an E",
-    subTitle:"Drama",
-    desc:"The adventures of a young orphan girl living in the late 19th century. Follow Anne as she learns to navigate her new life on Prince Edward Island, in this new ...",
- videoURL:"videos/6.mp4"
+  5: {
+    title: "Anne with an E",
+    subTitle: "Drama",
+    desc: "The adventures of a young orphan girl living in the late 19th century. Follow Anne as she learns to navigate her new life on Prince Edward Island, in this new ...",
+    videoURL: "videos/6.mp4"
   },
 }
 
@@ -89,19 +89,24 @@ const swiper = new Swiper('.bannerSwiper', {
 
 
 //Change titles
-let changeTitles=(index)=>{
-let title=document.querySelector("#title");
-let subTitle=document.querySelector("#sub-title");
-let desc=document.querySelector("#desc");
+let changeTitles = (index) => {
+  let title = document.querySelector("#title");
+  let subTitle = document.querySelector("#sub-title");
+  let desc = document.querySelector("#desc");
 
-title.innerHTML=`<h1>${titles[index].title}</h1>`
+  title.innerHTML = `<h1>${titles[index].title}</h1>`;
+  subTitle.innerHTML = `<p>${titles[index].subTitle}</p>`;
+  desc.innerHTML = `<p>${titles[index].desc}</p>`;
 }
+swiper.on('activeIndexChange', function () {
+  changeTitles(swiper.activeIndex);
+})
 //Show overlay
-let overlay=document.querySelector(".overlay");
-let showTrailer=()=>{
+let overlay = document.querySelector(".overlay");
+let showTrailer = () => {
   overlay.classList.add("show");
 }
 // close overlay
-let closeOverlay=()=>{
+let closeOverlay = () => {
   overlay.classList.remove("show");
 }
